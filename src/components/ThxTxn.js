67ca7ac895aba57639c1f49fc62294b316ef7811
shun takagi <s.takagi.910.dammy@gmail.com>
2019-04-17 02:19:45 +0900

    update header

--- src/components/ThxTxn.js
+++ src/components/ThxTxn.js
@@ -1,6 +1,6 @@
 import React, {Component} from "react";
 import '../css/ThxTxn.css'
-import ThxSmallAvatar from "./ThxSmallAvatar";
+import ThxSmallAvatar from "./ThxNomalAvatar";
 import ThxBigAvatar from './ThxBigAvatar'
 
 class ThxTxn extends Component {
